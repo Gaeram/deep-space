b3ee70624e986780d771b8ba45f3a88f20760a14
Tyranikwerewolf <83700003+Gaeram@users.noreply.github.com>
2022-07-27 09:50:35 +0200

    adding apache pack

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,7 +2,14 @@
 
 namespace App\Controller;
 
-class PageController
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PageController extends AbstractController
 {
+    #[Route("/", name:"home")]
+    public function homePage(){
+        return $this->render("index.html.twig");
+    }
 
 }
