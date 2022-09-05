5d0375ea7fa1db1d5b9fd70655e718fc4482b507
gaeram <gae.ram@icloud.com>
2022-09-05 15:52:27 +0200

    Beginning signup page

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,7 +2,29 @@
 
 namespace App\Controller;
 
-class UserController
-{
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use Symfony\Config\Security\PasswordHasherConfig;
+use App\Form\UserType;
 
+class UserController extends AbstractController
+{
+    #[Route ("/signup", name: "sign-up")]
+    public function index(UserPasswordHasherInterface $passwordHasher)
+    {
+        // ... e.g. get the user data from a registration form
+        $user = new User();
+        $plaintextPassword =  "mot de passe";
+
+        // hash the password (based on the security.yaml config for the $user class)
+        $hashedPassword = $passwordHasher->hashPassword(
+            $user,
+            $plaintextPassword
+        );
+        $user->setPassword($hashedPassword);
+
+        // ...
+    }
 }
