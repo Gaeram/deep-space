4c95ca6292e5e78cf96918b13b1a0fc21aa4560c
gaeram <gae.ram@icloud.com>
2022-09-05 17:23:17 +0200

    working on signup form

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,24 +6,33 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Config\Security\PasswordHasherConfig;
 use App\Form\UserType;
 
 class UserController extends AbstractController
 {
-    #[Route ("/signup", name: "sign-up")]
-    public function index(UserPasswordHasherInterface $passwordHasher)
+    #[Route ("login/signup", name: "sign-up")]
+    public function index(UserPasswordHasherInterface $passwordHasher, AuthenticationUtils $authenticationUtils)
     {
         // ... e.g. get the user data from a registration form
         $user = new User();
         $plaintextPassword =  "mot de passe";
+        $error = $authenticationUtils->getLastAuthenticationError();
 
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
         // hash the password (based on the security.yaml config for the $user class)
         $hashedPassword = $passwordHasher->hashPassword(
             $user,
             $plaintextPassword
         );
         $user->setPassword($hashedPassword);
+
+        return $this->render("login/signup.html.twig",[
+            'last_username' => $lastUsername,
+            'error' => $error
+            ]);
 
         // ...
     }
