9b0edb1203311caaa5b484520bbdc4d78af37089
gaeram <gae.ram@icloud.com>
2022-09-08 00:31:00 +0200

    Beginning final step of my exam project : Comment space

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -4,19 +4,40 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Entity\Comment;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Form\CommentFormType;
 use App\Repository\CommentRepository;
 use App\Repository\ArticleRepository;
+use Symfony\Component\Routing\Annotation\Route;
 
 class CommentController extends AbstractController
+
+
 {
-        public function Comment(Request $request, Article $article, CommentRepository $commentRepository): Response {
+        #[Route("/article", name: "article")]
+        public function Comment($id,EntityManagerInterface $entityManager,Request $request, CommentRepository $commentRepository, ArticleRepository $articleRepository) {
 
             $comment = new Comment();
 
             $form = $this->createForm(CommentFormType::class, $comment);
 
             $form->handleRequest($request);
+
+            $articleRepository->find($id);
+
+            $commentRepository->findAll();
+
+            if ($form->isSubmitted()){
+                $entityManager->persist($comment);
+                $entityManager->flush();
+
+                return $this->redirectToRoute("article-page");
+            }
+
+            return $this->render("article.html.twig",[
+                "comment"=>$comment
+            ]);
+
         }
 }
