b53fa35782c76ae6d650877345fcf3e39879d841
gaeram <gae.ram@icloud.com>
2022-09-07 15:54:07 +0200

    route for sign up added in menu

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -5,8 +5,16 @@ namespace App\Controller;
 use App\Entity\Comment;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Form\CommentFormType;
+use App\Repository\CommentRepository;
+use App\Repository\ArticleRepository;
 
 class CommentController extends AbstractController
 {
+        public function Comment(Request $request, Conference $conference, CommentRepository $commentRepository): Response {
 
+            $comment = new Comment();
+
+            $form = $this->createForm(CommentFormType::class, $comment);
+
+        }
 }
